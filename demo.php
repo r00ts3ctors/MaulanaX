95521fd76bc75efe58ecff5d74f601f2f1e28612
Bang Maul <r00t.s3ctors@gmail.com>
2022-06-26 20:59:13 +0700

    Update Code

--- demo.php
+++ demo.php
@@ -1,1 +1,56 @@
 <?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class  User extends CI_Controller {
+  
+  
+  public function __construct()
+  {
+    parent::__construct();
+    
+    
+  }
+  
+  // menampilkan seluruh data anggota
+  public function index()
+  {
+    
+  }
+
+  // menampilkan detail anggota
+  public function detailAnggota($id)
+  {
+    # code...
+  }
+
+  public function tambahAnggota()
+  {
+    
+  }
+
+
+  public function prosesTambah()
+  {
+    # code...
+  }
+
+  public function editAnggota($id)
+  {
+    # code...
+  }
+
+  public function updateAnggota()
+  {
+    # code...
+  }
+
+  public function hapus($id)
+  {
+    # code...
+  }
+
+
+}
+
+/* End of file  User.php */
